23b348df3fa6329b3485c24e67194cddfbfacc59
DESKTOP-4S0KHNG\alexp <alexkountach7@gmail.com>
2025-09-25 11:56:34 +0200

    ejercicio primer proyecto intellj

---
+++ ejercicios_alex/src/main/java/org/example/proyecto.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class proyecto {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("bienvenido Alex");
+
+    }
+}
