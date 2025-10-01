a175fa188a2dcf6b98fe8aba34f2f368e6de081d
DESKTOP-4S0KHNG\alexp <alexkountach7@gmail.com>
2025-10-01 19:48:08 +0200

    hacer ultimas revisiones de la practica 3

--- ejercicios_alex/src/main/java/org/example/practica_3_alex.java
+++ ejercicios_alex/src/main/java/org/example/practica_3_alex.java
@@ -6,30 +6,36 @@ import java.util.Scanner;
 public class practica_3_alex {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double altura=0;
-        double peso=0;
+        double altura = 0;
+        double peso = 0;
         double pesominimo;
         double totalpeso;
         double pesomax;
+        double pesomaxconst = 120;
+        double alturamaxconst = 230;
+        double alturaminconst = 140;
         System.out.println("------------------------------");
         System.out.println("***Bienvenido al raton jugueton***");
         System.out.println("------------------------------");
-        try {
-            System.out.println("introduce tu altura");
-            altura = sc.nextDouble();
-        } catch (InputMismatchException err) {
-            System.out.println("introduce un valor valido");
-            return;
-        }
-        System.out.println("leyendo altura (cm) ......");
-        if (altura > 230 || altura <= 0) {
-            System.out.println("Error de lectura, baja de la bascula y vuelve a subir...");
-        } else if (altura < 140) {
-            double diferenciacm = 140 - altura;
-            System.out.println("para subir a la atraccion te faltan: " + diferenciacm + "cm");
-        } else {
-            System.out.println("altura correcta");
-        }
+        do {
+            try {
+                System.out.println("introduce tu altura");
+                altura = sc.nextDouble();
+            } catch (InputMismatchException err) {
+                System.out.println("introduce un valor valido");
+                return;
+            }
+            System.out.println("leyendo altura (cm) ......");
+            if (altura > alturamaxconst || altura <= 0) {
+                System.out.println("Error de lectura, baja de la bascula y vuelve a subir...");
+            } else if (altura < alturaminconst) {
+                double diferenciacm = alturaminconst - altura;
+                System.out.println("para subir a la atraccion te faltan: " + diferenciacm + "cm");
+            } else {
+                System.out.println("altura correcta");
+            }
+        } while (altura > alturamaxconst || altura <= 0 || altura < alturaminconst);
+
         try {
             System.out.println("introduce tu peso");
             peso = sc.nextDouble();
@@ -38,18 +44,21 @@ public class practica_3_alex {
             return;
         }
         System.out.println("Leyendo peso....");
-        pesominimo = (peso * 2 / 8);
+        pesominimo = (altura * 2 / 8);
         if (peso < pesominimo) {
-            totalpeso = peso - pesominimo;
-            System.out.println("Lo siento, no superas el peso minimo requerido para subir a la atraccion");
-        } else if (peso > 120) {
-            pesomax = peso - 120;
-            System.out.println("Lo sineto, sobrepasas en " + pesomax + " kg el limite de la atraccion");
+            totalpeso = pesominimo - peso;
+            System.out.println("Lo siento, no superas el peso minimo, te faltan " + totalpeso + " kg para subir a la atraccion");
+        } else if (peso > pesomaxconst) {
+            pesomax = peso - pesomaxconst;
+            System.out.println("Lo siento, sobrepasas en " + pesomax + " kg el limite de la atraccion");
 
         } else {
             System.out.println("pesas " + peso + " kg, peso minimo calculado: " + pesominimo);
             System.out.println("¡¡Sube a la atraccion!!");
         }
-
     }
 }
+
+
+
+
